e893491cb2106092fd05b424f32edd31dfca296a
bestestest kitteh <236551@edu.p.lodz.pl>
2023-10-19 23:41:39 +0200

    refactor meowperhension testing

--- test/mrrrp/meowperhonsion_test.clj
+++ test/mrrrp/meowperhonsion_test.clj
@@ -1,43 +1,47 @@
 (ns mrrrp.meowperhonsion-test
   (:require
-   [clojure.test :refer [deftest is are]]
+   [clojure.test :refer [deftest is are testing]]
    [mrrrp.meowperhonsion :refer [extract-meows strip answer] :as c]))
-
-(binding [c/append-catface str]
-  (deftest stripper-test
-    (are [in out] (= [in out]
-                     [in (c/strip-trailing-catface in)])
-      "mraa mrrrp" "mraa mrrrp"
-      "meow :3" "meow"
-      "mraow :3 " "mraow"
-      "meow :3 meow" "meow :3 meow"
-      "Gods, i meow so much" "Gods, i meow so much"))
-  (deftest extract-meows-test-single
-    (are [in out] (= [in out]
-                     (cons in (extract-meows in)))
-      "meow" "meow"
-      "mraww" "mraww"
-      "mraa mrrrp" "mraa mrrrp"
-      "meow :3" "meow"))
-  (deftest it-knows-what-catfaces-arent
-    (are [in out] (= [in out]
-                     [in (answer in)])
-      ":3" [":3"]
-      "" []
-      "randomword:3" [":3"]
-      "unwanted:pattention" []
-      "<:patpat:1089585540388626502>" []
-      ":p" [":p"]))
-  (deftest it-behaves
-    (are [in out] (= [in out]
-                     [in (answer in)])
-      "coś ma" []
-      "hey Ma!" []
-      "touch me" []
-      "kitty: cat" []
-      "poor" []))
-  (deftest it-isn't-caninephobic
-    (are [in out] (= [in out]
-                     [in (answer in)])
-      "awooooomnia" ["awooooo"]
-      "woof!" ["woof!"])))
+(defmacro meowbacks [& pairs]
+  `(are [in out] (= [in out]
+                    [in (answer in)]) ~@pairs))
+(deftest meowperhension-test-no-appending
+  (binding [c/append-catface str]
+    (testing "stripper-test"
+      (are [in out] (= [in out]
+                       [in (c/strip-trailing-catface in)])
+        "mraa mrrrp" "mraa mrrrp"
+        "meow :3" "meow"
+        "mraow :3 " "mraow"
+        "meow :3 meow" "meow :3 meow"
+        "Gods, i meow so much" "Gods, i meow so much"))
+    (testing "extract-meows-test-single"
+      (are [in out] (= [in out]
+                       (cons in (extract-meows in)))
+        "meow" "meow"
+        "mraww" "mraww"
+        "mraa mrrrp" "mraa mrrrp"
+        "meow :3" "meow"))
+    (testing "it-knows-what-catfaces-arent"
+      (meowbacks
+       ":3" [":3"]
+       "" []
+       "randomword:3" [":3"]
+       "unwanted:pattention" []
+       "<:patpat:1089585540388626502>" []
+       ":p" [":p"]))
+    (testing "it-behaves"
+      (meowbacks
+       "coś ma" []
+       "hey Ma!" []
+       "touch me" []
+       "kitty: cat" []
+       "poor" []))
+    (testing "it-isn't-caninephobic"
+      (meowbacks
+       "awooooomnia" ["awooooo"]
+       "woof!" ["woof!"]))
+    (testing "stops-awo-ing"
+      (meowbacks
+       "wordawo" []
+       "prawo" []))))
