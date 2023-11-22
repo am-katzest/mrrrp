000a1f7dd286672994a8ee9290deab179b727e64
bestestest kitteh <236551@edu.p.lodz.pl>
2023-11-22 22:51:50 +0100

    begining of interceptorizing message-handler (wip)

--- src/mrrrp/message_handler.clj
+++ src/mrrrp/message_handler.clj
@@ -2,9 +2,48 @@
   (:require [mrrrp.effects :as fx]
             [mrrrp.finite-state-meowshine :as fsm]
             [clojure.tools.logging :as log]
+            [io.pedestal.interceptor :as int]
+            [io.pedestal.interceptor.chain :as chain]
             [better-cond.core :as b]
             [mrrrp.gayboy :as g]
             [mrrrp.slowdown :as slow]))
+
+;; input context map:
+#_{:event-data  {}                      ; what discord gives
+   :config {}
+   :state {}}                           ; modifications are preserved
+
+
+(def terminators [(fn [context] (not-empty (:fx context)))])
+
+(def always-meow
+  {:enter
+   (fn [context]
+     (assoc context :fx [[:reply "meow"]]))})
+
+(def unpack-message
+  {:enter
+   (fn [{:keys [event] :as context}]
+     (assoc context :message
+            {:author (:id (:author event))
+             :channel (:channel-id event)
+             :content (:content event)}))})
+
+(defn add-fx-context [channel fx]
+  (case (first fx)
+    :reply {:type :reply :channel channel :text (str (second fx))}
+    fx))
+
+(def postprocess-replies
+  {:leave
+   (fn [context]
+     (update context :fx (fn [fx]
+                           (map (partial add-fx-context (-> context :message :channel)) fx))))})
+
+(def interceptors (->> [unpack-message
+                        postprocess-replies
+                        always-meow]
+                       (map int/interceptor)))
 
 (def blacklist (atom #{}))
 
@@ -21,27 +60,35 @@
    :uid uid
    :msg msg})
 
-(defn add-fx-context [channel fx]
-  (case (first fx)
-    :reply {:type :reply :channel channel :text (str (second fx))}
-    fx))
 
 (defn handle-message
   "takes state, config and message and returns updated state and fxs"
-  [{:keys [bot-id]} state {:keys [channel-id content author]}]
-  (b/cond
-    (= content "stop meowing") (stop-meowing channel-id)
-    (= content "start meowing") (start-meowing channel-id)
-    :when (not (@blacklist channel-id))
-    :when (not= bot-id (:id author))
-    :do (g/maybe-update-gayboy-meowing-area (:id author) channel-id content)
-    :when (or (g/not-gayboy (:id author)) (> 0.1 (rand)))
-    :when  (not (and (g/gayboy-in-channel? channel-id)
-                     (g/is-gayboy-able-to-handle-this-message? content)))
-    [state
-     (->>
-      (prepare-event channel-id (:id author) content)
-      fsm/accept-message!
-      (map (partial add-fx-context channel-id)))]))
+  [config state event]
+  (chain/execute
+   {::chain/terminators terminators
+    :event event
+    :state state
+    :config config}
+   interceptors))
+;; (handle-message {} initial-state {})
 
-(def initial-state {})
+#_(defn handle-message-old
+    "takes state, config and message and returns updated state and fxs"
+    [{:keys [bot-id]} {:keys [blacklist]  :as state} {:keys [channel-id content author]}]
+    (b/cond
+      :do (println "aaa-" blacklist)
+      (= content "stop meowing") (stop-meowing channel-id)
+      (= content "start meowing") (start-meowing channel-id)
+      :when (not (blacklist channel-id))
+      :when (not= bot-id (:id author))
+      :do (g/maybe-update-gayboy-meowing-area (:id author) channel-id content)
+      :when (or (g/not-gayboy (:id author)) (> 0.1 (rand)))
+      :when  (not (and (g/gayboy-in-channel? channel-id)
+                       (g/is-gayboy-able-to-handle-this-message? content)))
+      [state
+       (->>
+        (prepare-event channel-id (:id author) content)
+        fsm/accept-message!
+        (map (partial add-fx-context channel-id)))]))
+
+(def initial-state {:blacklist #{}})
--- test/mrrrp/message_handler_test.clj
+++ test/mrrrp/message_handler_test.clj
@@ -2,10 +2,11 @@
   (:require [mrrrp.message-handler :as s]
             [clojure.test :refer :all]))
 
-(def state {})
+(def state s/initial-state)
 (def conf {:bot-id "self"})
 
-(defn wrap-msg "prepares message map" [channel author content]
+(defn wrap-msg "prepares message map, inverse of the `unpack-message` interceptor"
+  [channel author content]
   {:channel-id channel
    :content content
    :author {:id author}})
@@ -23,8 +24,8 @@
          [msg & rest] messages
          acc []]
     (if (nil? msg) {:state state :replies acc}
-        (let [[state' output]
-              (s/handle-message state config msg)]
+        (let [{state' :state output :fx}
+              (s/handle-message config state msg)]
           (recur state' rest (conj acc output))))))
 
 (defn output-texts [{:keys [replies]}]
@@ -32,21 +33,63 @@
 
 (defn in->out [config msg]
   (->> msg
-       (s/handle-message state config)
-       second
+       (s/handle-message config s/initial-state )
+       :fx
        (map :text)))
+
+(def context1 {:event (wrap-msg "chan1" "user1" "meow!")
+               :state s/initial-state
+               :config conf})
+(def context2 {:event (wrap-msg "chan1" "user1" "human stuff")
+               :message {:author "user1"
+                         :channel "chan1"
+                         :content "meow!"}
+               :state s/initial-state
+               :config conf})
+
+(deftest formatting-test
+  (is (= {:author "user1"
+          :channel "chan1"
+          :content "meow!"} (:message ((:enter s/unpack-message) context1)))))
+
+(deftest output-formatting-test
+  (is (= [] (:fx ((:leave s/postprocess-replies) (assoc context2 :fx nil)))))
+  (is (is (= [{:type :reply, :channel "chan1", :text "meow"}
+              {:type :reply, :channel "chan1", :text "uwu"}]
+             (:fx ((:leave s/postprocess-replies) (assoc context2 :fx [[:reply "meow"]
+                                                                     [:reply "uwu"]])))))))
+
 (deftest msg-handler-test
-  (is (= [state []] (s/handle-message
-                     conf state
-                     {:channel-id "channel"
-                      :content "--"
-                      :author {:id "user"}})))
-  (is (= [state [{:type :reply
-                  :channel "channel"
-                  :text "meow!"}]] (s/handle-message
-                                    conf state
-                                    {:channel-id "channel"
-                                     :content "meow!"
-                                     :author {:id "user"}})))
-  (is (= [] (in->out conf (wrap-msg "channel" "user" "normal words"))))
-  (is (= ["meow!"] (in->out conf (wrap-msg "channel" "user" "meow!")))))
+  (is (= [] (:fx (s/handle-message
+              conf s/initial-state
+              {:channel-id "channel"
+               :content "--"
+               :author {:id "user"}}))))
+    (is (= [{:fx :reply
+             :channel "channel"
+             :text "meow!"}] (:fx (s/handle-message
+                               conf s/initial-state
+                               {:channel-id "channel"
+                                :content "meow!"
+                                :author {:id "user"}}))))
+    (is (= [] (in->out conf (wrap-msg "channel" "user" "normal words"))))
+    (is (= ["meow!"] (in->out conf (wrap-msg "channel" "user" "meow!"))))
+    (testing "blacklist"
+      (testing "full interaction"
+        (is (= [["meow!"] [] [] [] ["meow!"]]
+               (->> ["meow!" "stop meowing" "meow!" "start meowing" "meow!"]
+                    (user-in-channel "user" "channel" )
+                    (run-messages-through conf)
+                    (output-texts)))))
+      (testing "blacklisting"
+        (is (= [["meow!"] [] []]
+               (->> ["meow!" "stop meowing" "meow!"]
+                    (user-in-channel "user" "channel" )
+                    (run-messages-through conf)
+                    (output-texts)))))
+      (testing "unblacklisting when no blacklist"
+        (is (= [["meow!"] [] ["meow!"]]
+               (->> ["meow!" "start meowing" "meow!"]
+                    (user-in-channel "user" "channel" )
+                    (run-messages-through conf)
+                    (output-texts)))))))
