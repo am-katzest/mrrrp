a969a15758c810189133827878646fa88877521b
bestestest kitteh <236551@edu.p.lodz.pl>
2024-01-08 01:54:58 +0100

    applying rules

--- src/mrrrp/rules.clj
+++ src/mrrrp/rules.clj
@@ -1,5 +1,6 @@
 (ns mrrrp.rules
-  (:require [malli.core :as m]))
+  (:require [malli.core :as m]
+            [mrrrp.meowperhonsion :as re]))
 
 (defn- str-set-limiter [name]
   [name {:optional true} [:set :string]])
@@ -12,7 +13,7 @@
    [:map
     [:text [:fn regex?]]
     (str-set-limiter :only-channels)
-    (str-set-limiter :only-users)
+    (str-set-limiter :only-authors)
     (str-set-limiter :only-servers)]))
 
 (def action-schema (m/schema [:tuple [:= :reply] string?]))
@@ -21,3 +22,30 @@
   (m/schema [:map
              [:condition condition-schema]
              [:action action-schema]]))
+
+(defn applies? [context rule]
+  (every? (fn [[key value]]
+            (case key
+              :only-channels (-> context :message :channel value)
+              :only-authors (-> context :message :author value)
+              :only-servers (-> context :event :guild-id value)
+              :text (->> context :message :content (re-matches value))))
+          (:condition rule)))
+
+(defmulti action first)
+
+(defmethod action :reply
+  [[_ text]]
+  [:reply text])
+
+(defn actions [context rule]
+  (when (applies? context rule)
+    [(action (:action rule))]))
+
+(def apply-rules-interceptor
+  {:enter (fn [context]
+            (let [rules (-> context :config :rules)
+                  actions (mapcat #(actions context %) rules)]
+              (if (empty? actions)
+                context
+                (assoc context :fx actions))))})
--- test/mrrrp/rules_test.clj
+++ test/mrrrp/rules_test.clj
@@ -4,9 +4,9 @@
             [clojure.test :refer [testing is deftest]]))
 
 (def rule1
-  {:condition {:only-channels #{"some"}
-               :only-users #{"a"}
-               :text #"^text$"}
+  {:condition {:only-channels #{"chan" "other"}
+               :only-authors #{"author"}
+               :text #"^meow$"}
    :action [:reply "reply"]})
 
 (def rule2
@@ -20,3 +20,47 @@
     (is (m/validate condition-schema (:condition rule2)))
     (is (m/validate action-schema (:action rule2))))
   (is (m/validate rule-schema rule1)))
+
+(def context1 {:event {:guild-id "guild"}
+               :message {:author "author"
+                         :content "meow"
+                         :channel "chan"}})
+
+(deftest rule-predicate-test
+  (testing "text"
+    (is (applies? context1 {:condition {:text #".*"}}))
+    (is (not (applies? context1 {:condition {:text #"wrong"}}))))
+  (testing "server"
+    (is (applies? context1 {:condition {:text #".*" :only-servers #{"guild"}}}))
+    (is (not (applies? context1 {:condition {:text #".*" :only-servers #{"wrong"}}}))))
+  (testing "channel"
+    (is (applies? context1 {:condition {:text #".*" :only-channels #{"chan"}}}))
+    (is (not (applies? context1 {:condition {:text #".*" :only-channels #{"wrong"}}}))))
+  (testing "author"
+    (is (applies? context1 {:condition {:text #".*" :only-authors #{"author"}}}))
+    (is (not (applies? context1 {:condition {:text #".*" :only-authors #{"wrong"}}}))))
+  (testing  "mixed"
+    (is (applies? context1 {:condition {:text #".*" :only-authors #{"author" "other" "blah"} :only-channels #{"chan"}}}))
+    (is (not (applies? context1 {:condition {:text #".*" :only-servers #{"wrong"} :only-channels #{"chan"}}})))))
+
+(deftest rule-applying-test
+  (is (= nil (actions context1 rule2)))
+  (is (= [[:reply "reply"]] (actions context1 rule1))))
+
+(deftest context-manipulation-test
+  (is (= [[:reply "reply"]]
+         (->> (assoc context1 :config {:rules [rule1]})
+              ((:enter apply-rules-interceptor))
+              :fx)))
+  (is (= [[:reply "reply"]]
+         (->> (assoc context1 :config {:rules [rule1 rule2]})
+              ((:enter apply-rules-interceptor))
+              :fx)))
+  (is (= [[:reply "reply"] [:reply "reply"]]
+         (->> (assoc context1 :config {:rules [rule1 rule1]})
+              ((:enter apply-rules-interceptor))
+              :fx)))
+  (is (= nil
+         (->> (assoc context1 :config {:rules [rule2 rule2]})
+              ((:enter apply-rules-interceptor))
+              :fx))))
